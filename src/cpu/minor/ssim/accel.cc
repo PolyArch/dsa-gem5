3fc97f8bf332077ae109bfb61963170fee933bf1
Jian Weng <jian.weng@ucla.edu>
2018-10-10 13:51:45 -0700

    make it compat to simplified ss-scheduler
    Change-Id: I249dad2ed5d36121cb88733e90901ea09f5be1ca

--- src/cpu/minor/ssim/accel.cc
+++ src/cpu/minor/ssim/accel.cc
@@ -924,7 +924,7 @@ void accel_t::cycle_cgra_backpressure() {
 
   if(in_roi()) {
     _stat_sb_insts+=num_computed;
-    _stat_sb_dfg_util+=(double)num_computed/_pdg->num_insts();
+    _stat_sb_dfg_util+=(double)num_computed/_pdg->ordered_insts().size();
   }
 
   // pop the ready outputs
